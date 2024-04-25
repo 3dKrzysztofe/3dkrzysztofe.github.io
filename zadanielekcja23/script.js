1f5717065daca6b41a28e5a7b09b78655b0b9cf0
Krzysztof Elmrych <114151661+3dKrzysztofe@users.noreply.github.com>
2024-04-25 23:16:19 +0200

    Add files via upload

---
+++ zadanielekcja23/script.js
@@ -0,0 +1,4 @@
+let oferta=document.getElementById("oferta")
+oferta.addEventListener("click", () => {
+    window.open("https://3dkrzysztofe.github.io/zadanielekcja23/KawiarnaMors.txt",'_blank')
+})
